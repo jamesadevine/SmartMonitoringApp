a7488d5276aa972350fcf4ece3d1d953bbff5a65
gunmania0 <james@devine.eu>
2015-01-17 20:50:35 +0000

    Added new charts about to phase out old ones

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -127,6 +127,14 @@ angular.module('smartfuse.controllers', ['smartfuse.api'])
 })
 .controller('HomeCtrl', function($scope, $ionicModal, $timeout,FuseAPI, UserAPI,$ionicLoading,$state,$ionicPopup,UserService,CacheService) {
     
+
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+
     var summaryTitle= "Ave. Energy Consumption:";
     
     var currentDate = moment().format("DD-MM-YYYY");
